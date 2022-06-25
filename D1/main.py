765727c3e31afe4bbd00e063c9b62eb60e8fdf02
Agata <s24305@pjwstk.edu.pl>
2022-06-25 12:13:35 +0200

    Day 1 - Band name generator

---
+++ D1/main.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator!")
+city = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is pet's name?\n")
+print("Your band name is " + city + " " + pet_name)
+
